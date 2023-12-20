878a2fa53adf10c6e724da3e9fcfe27a4adffb4c
Sarahprajna <148427095+Sarahprajna@users.noreply.github.com>
2023-12-20 23:12:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains import RetrievalQA
+from langchain.vectorstores import Chroma
+from langchain.docstore.document import Document
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+import torch
+import chromadb
+from transformers import AutoModel
+from chromadb.config import Settings
+from transformers import pipeline
+
 device = torch.device('cpu')
 access_token = "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"
 
